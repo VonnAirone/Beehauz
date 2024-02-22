df6d7f6b39dacf5de27939c8bc1e937e900ffe78
jezzamae <villasoraironevonn@gmail.com>
2024-02-22 08:23:41 +0800

    fifth commit - revised the account page and added a modal for the logout function

--- app/(tenant)/(tabs)/account.tsx
+++ app/(tenant)/(tabs)/account.tsx
@@ -1,96 +1,210 @@
-import { Pressable, SafeAreaView, Text, View, Image } from 'react-native'
-import React from 'react'
+import { Pressable, SafeAreaView, Text, View, Modal, Alert, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons'
-import Divider from 'react-native-divider'
 import Logo from '@/components/logo'
-import { router } from 'expo-router'
+import ModalScreen from '@/app/modal';
 
 export default function Account() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  
   return (
-    <SafeAreaView className='flex-1 w-96 justify-center m-auto'>
-      <View className='absolute top-0 left-72 opacity-10'>
-        <Image className='h-60 w-60' source={require("assets/honeyv3.png")}/>
-      </View>
-      <View className='items-center p-3'>
-        <View className='bg-gray-300 h-20 w-20 rounded-full mb-3'></View>
-        
-        <View className='ml-3 items-center'>
-          <Text className='font-semibold text-2xl mr-1'>Username</Text>
-          <View className='flex-row items-center '>
-            <Ionicons name='location-outline' size={20}/>
-            <Text>Location of the user</Text>
-          </View>
-        </View>
+    <SafeAreaView className='flex-1 w-80 justify-center m-auto'>
+      <View className='items-start -left-4'>
+        <Logo/>
       </View>
 
-      <Divider orientation="center"></Divider>
+      <View className='items-center mb-10'>
+        <View className='bg-slate-300 h-20 w-20 rounded-full mb-2'></View>
+        <Text className='text-xl font-semibold'>Airone Vonn Villasor</Text>
+      </View>
 
-      <View className='items-center w-80 mx-auto'>
-        <Text className='text-center'>Welcome to Beehauz! It looks like you're not on board yet. Let's find the perfect place for you to stay. Explore our listings and discover comfortable accommodations that suit your preferences. Start your journey now!</Text>
-
-        <Pressable 
-        // onPress={() => router.push('/(tenant)/(tabs)/home')}
-        android_ripple={{color: 'yellow'}} 
-        className='bg-yellow-500 p-3 rounded-sm flex-row items-center mt-3'>
-          <Text>Find now</Text>
-          <Ionicons name='chevron-forward-outline'/>
+      <View className='flex-row justify-between'>
+        <Text className='text-base text-yellow-600'>Personal Information</Text>
+        <Pressable className='flex-row items-center gap-x-1'>
+          <Ionicons name='pencil'/>
+          <Text>Edit</Text>
         </Pressable>
       </View>
 
-      <Divider orientation="center"></Divider>
-
-      <View className='items-center gap-3'>
-
-        {/* ACCOUNT MANAGEMENT */}
-        <View className='flex-row w-80 h-20 items-center p-5 border border-gray-200 rounded-md relative'>
-          <Ionicons name='person-outline' color={"#f8b000"} size={32}/>
-          <View className='ml-5'>
-            <Text className='font-semibold text-xl'>Account management</Text>
-            <Text className='text-xs'>Username, password, personal information</Text>
+      <View className='gap-y-3 mt-3 mb-10'>
+        <View className='flex-row justify-between p-2'>
+          <View className='flex-row items-center gap-x-2 opacity-60'>
+            <Ionicons name='mail-outline' size={18}/>
+            <Text className='text-base'>Email</Text>
           </View>
-          <View className='absolute right-3'>
-            <Ionicons name='chevron-forward-outline' size={28}/>
+
+          <View>
+            <Text>villasoraironevonn@gmail.com</Text>
           </View>
         </View>
 
-        {/* PAYMENT METHOD */}
-        <View className='flex-row w-80 h-20 items-center p-5 border border-gray-200 rounded-md relative'>
-          <Ionicons name='document-text-outline' color={"#f8b000"} size={32}/>
-          <View className='ml-5'>
-            <Text className='font-semibold text-xl'>Transaction History</Text>
-            <Text className='text-xs w-40'>Booking history, payment method, payment history</Text>
+        <View className='border-b opacity-10'></View>
+
+        <View className='flex-row justify-between p-2'>
+          <View className='flex-row items-center gap-x-2 opacity-60'>
+            <Ionicons name='phone-portrait-outline' size={18}/>
+            <Text className='text-base'>Phone</Text>
           </View>
-          <View className='absolute right-3'>
-            <Ionicons name='chevron-forward-outline' size={28}/>
+
+          <View>
+            <Text>09491488343</Text>
           </View>
         </View>
 
-        {/* HELP CENTER */}
-        <View className='flex-row w-80 h-20 items-center p-5 border border-gray-200 rounded-md relative'>
-          <Ionicons name='help-circle-outline' color={"#f8b000"} size={32}/>
-          <View className='ml-5'>
-            <Text className='font-semibold text-xl'>Help Center</Text>
-            <Text className='text-xs w-40'>FAQs, Policies and Terms of Service</Text>
+        <View className='border-b opacity-10'></View>
+
+        <View className='flex-row justify-between p-2'>
+          <View className='flex-row items-center gap-x-2 opacity-60'>
+            <Ionicons name='location-outline' size={18}/>
+            <Text className='text-base'>Location</Text>
           </View>
-          <View className='absolute right-3'>
-            <Ionicons name='chevron-forward-outline' size={28}/>
+
+          <View>
+            <Text>09491488343</Text>
           </View>
         </View>
-
-        {/* RATE THIS APP */}
-        <View className='flex-row w-80 h-20 items-center p-5 border border-gray-200 rounded-md relative'>
-          <Ionicons name='star-half-outline' color={"#f8b000"} size={32}/>
-          <View className='ml-5'>
-            <Text className='font-semibold text-xl'>Rate this App</Text>
-            <Text className='text-xs w-40'>Feedback, experiences, ratings</Text>
-          </View>
-          <View className='absolute right-3'>
-            <Ionicons name='chevron-forward-outline' size={28}/>
-          </View>
-        </View>
-
       </View>
+
+      <View>
+        <Text className='text-base text-yellow-600'>Utilities</Text>
+      </View>
+
+      <View className='gap-y-3 mt-3 mb-10'>
+        <Pressable android_ripple={{color: 'f1f1f1'}} className='p-2'>
+          <View className='flex-row justify-between items-center'>
+            <View className='flex-row items-center gap-x-2 opacity-60'>
+              <Ionicons name='document-text-outline' size={18}/>
+              <Text className='text-base'>Transaction History</Text>
+            </View>
+
+            <View>
+              <Ionicons name='chevron-forward-outline'/>
+            </View>
+          </View>
+        </Pressable>
+
+        <View className='border-b opacity-10'></View>
+
+        <Pressable android_ripple={{color: 'f1f1f1'}} className='p-2'>
+          <View className='flex-row justify-between items-center'>
+            <View className='flex-row items-center gap-x-2 opacity-60'>
+              <Ionicons name='information-circle-outline' size={18}/>
+              <Text className='text-base'>Help Center</Text>
+            </View>
+
+            <View>
+              <Ionicons name='chevron-forward-outline'/>
+            </View>
+          </View>
+        </Pressable>
+
+        <View className='border-b opacity-10'></View>
+
+        <Pressable android_ripple={{color: 'f1f1f1'}} className='p-2'>
+          <View className='flex-row justify-between items-center'>
+            <View className='flex-row items-center gap-x-2 opacity-60'>
+              <Ionicons name='star-half-outline' size={18}/>
+              <Text className='text-base'>Rate this app</Text>
+            </View>
+
+            <View>
+              <Ionicons name='chevron-forward-outline'/>
+            </View>
+          </View>
+        </Pressable>
+
+        <View className='border-b opacity-10'></View>
+
+        <Pressable
+        onPress={() => setModalVisible(true)}
+        android_ripple={{color: 'f1f1f1'}} 
+        className='p-2'>
+          <View className='flex-row justify-between items-center'>
+            <View className='flex-row items-center gap-x-2 opacity-60'>
+              <Ionicons name='log-out-outline' size={18}/>
+              <Text className='text-base'>Logout</Text>
+            </View>
+
+            <View>
+              <Ionicons name='chevron-forward-outline'/>
+            </View>
+          </View>
+        </Pressable>
         
+      </View>
+
+      
+      {/* MODAL AREA */}
+      
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+
+        <View className='flex-1 justify-center items-center'>
+          <View className='bg-white h-40 w-72 rounded-md justify-center'>
+
+            <View className='absolute top-2 right-3'>
+              <Ionicons name='close-outline' size={20}/>
+            </View>
+
+            <Text className='text-center font-semibold text-base mb-3'>Are you sure about logging out?</Text>
+            <Pressable
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
